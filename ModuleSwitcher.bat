2dd4f414f0f09c9efb632fb6738a4d5450d34bb6
Doug Baer <dbaer@vmware.com>
2018-02-16 12:50:40 -0700

    Adding documentation

--- ModuleSwitcher.bat
+++ ModuleSwitcher.bat
@@ -1,2 +1,3 @@
 @echo off
+REM Example ModuleSwitcher batch file that can be used to launch a module switcher instance
 C:\WINDOWS\system32\windowspowershell\v1.0\powershell.exe -windowstyle hidden "& 'C:\HOL\ModuleSwitcher.ps1'"
---
+++ README.md
@@ -0,0 +1,3 @@
+#hol-module-switcher
+
+A PowerShell-based panel used to launch module start/stop scripts for demonstration and training environments.
